c5aca8244edf072d3740970794d1a86300b96139
Artnod <artnod78@users.noreply.github.com>
2018-11-04 17:24:54 +0100

    Add files via upload

--- client/esx_journalist_cl.lua
+++ client/esx_journalist_cl.lua
@@ -268,7 +268,7 @@ Citizen.CreateThread(function()
   while isLoading do Citizen.Wait(10) end
   while true do
     Citizen.Wait(0)
-    if playerData.job.grade >= Config.journalistMinGrade and IsControlJustReleased(1, Keys["F7"]) and isWorking then openMobileweazelMenu() end
+    if playerData.job.grade >= Config.journalistMinGrade and IsControlJustReleased(1, Keys["F6"]) and isWorking then openMobileweazelMenu() end
     if currentAction ~= nil then
       SetTextComponentFormat('STRING')
       AddTextComponentString(currentActionMsg)
