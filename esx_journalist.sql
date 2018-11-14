78a613a6ced40823587228ddea4ae52c5806efcf
Artnod <artnod78@users.noreply.github.com>
2018-11-14 15:42:11 +0100

    Update esx_journalist.sql

--- esx_journalist.sql
+++ esx_journalist.sql
@@ -1,26 +1,26 @@
 USE `essentialmode`;
 
 INSERT INTO `jobs` (`name`, `label`, `whitelisted`) VALUES
-	('journalist', 'WeazelNews', 0)
+	('journalist', 'Weazel News', 0)
 ;
 INSERT INTO `datastore` (`name`, `label`, `shared`) VALUES
-	('society_weazel', 'WeazelNews', 1),
+	('society_weazel', 'Weazel News', 1),
 ;
 
 INSERT INTO `addon_account` (`name`, `label`, `shared`) VALUES
-	('society_weazel', 'WeazelNews', 1),
-	('society_taxe_weazel', 'WeazelNews Taxe', 1)
+	('society_weazel', 'Weazel News', 1),
+	('society_taxe_weazel', 'Weazel News Taxe', 1)
 ;
 
 INSERT INTO `addon_inventory` (`name`, `label`, `shared`) VALUES
-	('society_weazel', 'WeazelNews', 1)
+	('society_weazel', 'Weazel News', 1)
 ;
 
 INSERT INTO `job_grades` (`job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
 	('journalist', 0, 'interim', 'Livreur', 100, '{}', '{}'),
 	('journalist', 1, 'journalist', 'Journaliste', 100, '{}', '{}'),
 	('journalist', 2, 'reporter', 'Reporter', 100, '{}', '{}'),
-	('journalist', 3, 'chief', 'Rédacteur en chef', 100, '{}', '{}'),
+	('journalist', 3, 'chief', 'RÃ©dacteur en chef', 100, '{}', '{}'),
 	('journalist', 4, 'boss'  , 'Patron', 100, '{}', '{}')
 ;
 
